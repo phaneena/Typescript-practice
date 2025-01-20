41e70ab23b75192f8d20bff62fcd47f411ff5b53
Haneena <pphaneena02@gmail.com>
2025-01-20 14:09:17 +0530

    type-script

---
+++ sample.ts
@@ -0,0 +1,34 @@
+//infer type
+
+// let username="haneena"
+// username=20
+
+//defining type
+// let username:string="hani"
+// let age:number=20
+// let success:boolean=true
+// let skills:string[]=["html","css","js"]
+// let count:number[]=[1,2,3,4]
+// let userdetail:{name:string,age:number}={
+//     name:"haniii",
+//     age:20
+// }
+
+//interface
+
+// interface Details{
+//     name:string,
+//     age:number,
+//     salary:number,
+
+// }
+// let userDetails:Details={
+//     name:"hanii",
+//     age:22,
+//     salary:20000
+// }
+// console.log(userDetails)
+
+
+let skill:(string | number)[]=["html","css",12,34]
+console.log(skill)
